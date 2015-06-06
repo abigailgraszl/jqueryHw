73a3faf36ef0785c2292b7abe57c1ab9a5cde597
Abigail Graszl <acgraszl@gmail.com>
2015-06-05 20:52:50 -0400

    Html boiler plate. Finished jquery cheat sheet.

---
+++ javascript/scripts.js
@@ -0,0 +1,36 @@
+//loops
+for (x=0; x<10; x++);
+	console.log(x);
+
+var z=100;
+while(z<100){
+	console.log(z + "1zombie")
+}
+
+//switch
+var x=1;
+switch(x){
+	case 1;
+		console.log("1 is the winner");
+	break;
+	case 2;
+		console.log("2 is the winner");
+	break;
+	case 3;
+		console.log("3 is the winner");
+	break;
+}
+
+//jquery boiler plate
+$(document).ready(function(){
+
+
+});
+
+//selecting elements in jquery
+$("#redSq") //selecting by id
+$(".main") //selecting by class
+$("h1")  //selecting by tag
+
+
+
