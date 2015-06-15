a7d1dd3c22731af1387844bd2ab02c1b3f17255d
Abigail Graszl <acgraszl@gmail.com>
2015-06-14 20:53:03 -0400

    finished animations

--- javascript/sitescripts.js
+++ javascript/sitescripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
 
+	//var ItIsDay=true;
+
 	$("#daytime").click(function(){
 		$("#field").fadeIn();
 		$("#daytime").hide();
-	});
+		$("#nighttime").show();
+		$("#stars").hide();
+		$("#shootStar").hide();
+
 		setTimeout(function(){
 			$("#giraffe").fadeIn();
 		}, 2000);
@@ -15,7 +20,25 @@ $(document).ready(function(){
 		$("#feedMe").click(function(){
 			$("#tree").slideDown();
 		});
+	});
+
 	
+	$("#nighttime").click(function(){
+		$("#stars").fadeIn();
+		$("#nighttime").hide();
+		$("#daytime").show();
+		$("#field").hide();
+		$("#food").hide();
+
+		setTimeout(function(){
+			$("#shootStar").fadeIn()
+		}, 2000);
+
+		setTimeout(function(){
+			$("#shootStar").slideUp()
+		}, 4000);
+
+	});
 
 
 
